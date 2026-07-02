f02e92d4314240f665ac668a61b9575013375f0f
Rafael Siqueira <rafaelsiqueira100@gmail.com>
2026-07-02 16:51:30 -0300

    fixing 02 and 03

--- entrypoints/03-specular.py
+++ entrypoints/03-specular.py
@@ -9,27 +9,26 @@ import urenderer
 # Altere os arquivos .../shader_library/specular.glsl,
 # e 03-fragment.fs para realizar o sombreamento.
 
-if __name__ == "__main__":
-    urenderer.utils.clear_workdir("03-specular")
-    renderer = urenderer.renderer.OpenGLRenderer(1920, 1080)
-    renderer.background_color = np.array([0, 0, 0, 1], np.float32)
-    runtime = urenderer.application.Runtime(
-        renderer, name="03-specular")
+urenderer.utils.clear_workdir("03-specular")
+renderer = urenderer.renderer.OpenGLRenderer(1920, 1080)
+renderer.background_color = np.array([0, 0, 0, 1], np.float32)
+runtime = urenderer.application.Runtime(
+    renderer, name="03-specular")
 
-    shader = urenderer.renderer.Shader("vertex.vs", "03-fragment.fs")
-    material = urenderer.renderer.opengl.Material(shader)
+shader = urenderer.renderer.Shader("vertex.vs", "03-fragment.fs")
+material = urenderer.renderer.opengl.Material(shader)
 
-    sphere = urenderer.node.Node()
+sphere = urenderer.node.Node()
 
-    sphere.translation = np.array([0, 0, -5], np.float64)
-    sphere.render_data["mesh"] = urenderer.geometry.mesh.get_mesh_sphere()
-    sphere.render_data["material"] = material
+sphere.translation = np.array([0, 0, -5], np.float64)
+sphere.render_data["mesh"] = urenderer.geometry.mesh.get_mesh_sphere()
+sphere.render_data["material"] = material
 
-    runtime.scene.add_child(sphere)
+runtime.scene.add_child(sphere)
 
-    light = urenderer.node.Light(urenderer.node.LightType.DIRECTIONAL)
-    light.rotation = np.array([45, 45, 45], np.float64)
-    light.light_intensity = 3.0
-    runtime.scene.add_child(light)
+light = urenderer.node.Light(urenderer.node.LightType.DIRECTIONAL)
+light.rotation = np.array([45, 45, 45], np.float64)
+light.light_intensity = 3.0
+runtime.scene.add_child(light)
 
-    runtime.loop(capture=[1])
+runtime.loop(capture=[1])
